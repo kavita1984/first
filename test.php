2ff982e5780a69060209b3fbaeec9aebf80ce00e
kavita1984 <108350558+kavita1984@users.noreply.github.com>
2023-08-05 22:35:22 +0530

    Update test.php

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ wednesday => wenz day
 saturday => sa-tuh day
 woman => vu-mun
 women => vi-men
-determin => du-tuh-min
-handsome => han-some
+determin => du-tuh-min // for 1
+handsome => han-some // for more than 1
 ?>
