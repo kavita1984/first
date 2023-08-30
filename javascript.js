299ddf7a08a15686fbf039c0fd8e3e6067e8f78b
kavita1984 <108350558+kavita1984@users.noreply.github.com>
2023-08-30 19:22:35 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ let accountState;
 
 // accountId = 2 // not allowed
 
-
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
@@ -18,5 +17,34 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 
-
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
